1d50e543597196a6632e575fac5d7d96a719a880
Jeppe Johansen <Jeppe@Jeppes-MacBook-Pro.local>
2019-04-02 09:13:10 +0200

    added initialization

--- src/model.py
+++ src/model.py
@@ -2,9 +2,10 @@ from agent import Agent
 
 class Model(Agent):
 
-    def __init__(self):
-        self
+    def __init__(self, par, state):
+        super().__init__(par=par, state=state)
 
 
     @staticmethod
     def greate_grids():
+        pass
