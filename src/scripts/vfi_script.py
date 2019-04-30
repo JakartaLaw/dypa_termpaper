be7b153e109bf2d6473bced21caba7dc5f93e0e4
Ditlev Kiersgaard Frisch <ditlevkiersgaard@gmail.com>
2019-04-30 09:42:44 +0200

    changed vfi_script

--- src/scripts/vfi_script.py
+++ src/scripts/vfi_script.py
@@ -2,7 +2,7 @@
 # Brute force method of solving (inspired by EGM in Exercise 2, model.py)
 def find_V_bruteforce(f_t, m_t, p_t, tr_t, c_t, kappa_t, invest_t, t, par):
     '''Calculates E_t(V_t+1) via brute force looping'''
-    integrand = 0
+    V_fut = 0
     for j_psi in range(1, par.Npsi):
         for i_xi in range(1, par.Nxi):
             for k_eps in range(1, par.Neps):
@@ -11,8 +11,12 @@ def find_V_bruteforce(f_t, m_t, p_t, tr_t, c_t, kappa_t, invest_t, t, par):
                 assets = m_t + tr_t - c_t - cls.pi_cost(invest_t) - cls.kappa_cost(kappa_t)
                 income = par.xi[i_xi] * (par.G * p_t *  par.psi[j_psi] + cls.agepoly[t+1])
 
-                integrand += interest_factor * assets + income
-    return(integrand)
+                integrand = interest_factor * assets + income
+
+                V = par.psi_w * par.xi_w * par.eps_w * par.Vplus_interp(integrand) # GH weighting
+                V_fut += V
+
+    return(V_fut)
 
 
 # Pre-compute integral for each state (to avoid looping 8^3 times for each grid in state space and choice)
